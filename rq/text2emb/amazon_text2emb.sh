95e445f029e2360512b1bd78671a3071be4c23de
Chen Zhang <chen.zhang3@rwth-aachen.de>
2026-03-01 19:36:26 +0100

    Align encoder and num_processes with paper

--- rq/text2emb/amazon_text2emb.sh
+++ rq/text2emb/amazon_text2emb.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/env bash
-export HF_ENDPOINT=https://hf-mirror.com
-export HF_HOME=/root/autodl-tmp/hf
-export TRANSFORMERS_CACHE=/root/autodl-tmp/hf
 accelerate launch --num_processes 1 amazon_text2emb.py \
     --dataset Industrial_and_Scientific \
     --root ../../data/Amazon/index \
