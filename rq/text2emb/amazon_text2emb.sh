34d76c37bf5f6a7440019681845fcc35ea45e992
Chen Zhang <chen.zhang3@rwth-aachen.de>
2026-03-01 18:18:53 +0100

    Add HF mirror support, adjust paths and num_process

--- rq/text2emb/amazon_text2emb.sh
+++ rq/text2emb/amazon_text2emb.sh
@@ -1,4 +1,8 @@
-accelerate launch --num_processes 8 amazon_text2emb.py \
+#!/usr/bin/env bash
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_HOME=/root/autodl-tmp/hf
+export TRANSFORMERS_CACHE=/root/autodl-tmp/hf
+accelerate launch --num_processes 1 amazon_text2emb.py \
     --dataset Industrial_and_Scientific \
-    --root ../../data/Amazon18/Industrial_and_Scientific \
-    --plm_checkpoint your_emb_model_path
+    --root ../../data/Amazon/index \
+    --plm_checkpoint Qwen/Qwen3-Embedding-4B
